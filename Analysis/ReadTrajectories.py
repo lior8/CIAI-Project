c2269931a976e887363c896984e901be9e23924d
topder <62852782+topder@users.noreply.github.com>
2023-07-18 21:24:36 +0300

    Add files via upload

---
+++ Analysis/ReadTrajectories.py
@@ -0,0 +1,27 @@
+
+from Trace import Trace
+import pandas as pd
+import os
+
+def tajectories_to_csv(input_dir, output_dir):
+    for setting in os.listdir(input_dir):
+        curr_dir = input_dir + "\\" + setting
+        filenames = os.listdir(curr_dir)
+        for filename in filenames:
+            trace = Trace(input_dir + "\\" + setting + "\\" + filename)
+            for i, episode in enumerate(trace):
+                episode_dict = {}
+                for step in episode.steps:
+                    for j, action in enumerate(step.actions):
+                        if j not in episode_dict.keys():
+                            episode_dict[j] = []
+                        episode_dict[j].append(action)
+#
+                # win_df = win_df.append(pd.DataFrame({"is_win": episode.is_win()}, index=[i]))
+                episode_df = pd.DataFrame(episode_dict)
+                episode_df.to_csv(output_dir + setting + "_" + filename.split(".")[0] + str(i) + ".csv", header=False, index=False)
+
+
+input_dir = r"C:\University\Semester_10\Code\Collaboration_AI\output\SmacTrace\traces"
+output_dir = r"C:\University\Semester_10\Code\Collaboration_AI\output\SmacTrace\csvs\\"
+tajectories_to_csv(input_dir, output_dir)
